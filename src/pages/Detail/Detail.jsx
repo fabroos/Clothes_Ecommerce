3e98590243120b7abf0c1aa5e42057be8517cf4a
Fabrizio Signoretta <signorettafabrizio@gmail.com>
2022-03-26 10:21:18 -0300

    fix a route

--- src/pages/Detail/Detail.jsx
+++ src/pages/Detail/Detail.jsx
@@ -18,7 +18,7 @@ import { Header } from '../../components/Header/Header'
 import { CartContext } from '../../contexts/CartContext'
 import { useAsync } from '../../hooks/useAsync'
 import { getSingleProduct } from '../../services/getSingleProduct'
-import { ItemDetailPlaceholder } from './Components/ItemDetailPlaceholder/Detail'
+import { ItemDetailPlaceholder } from './Components/ItemDetailPlaceholder/ItemDetailPlaceholder'
 
 export function Detail () {
   const [quantity, setQuantity] = useState(1)
